b592c280d9506070ad6e5ff378de6d94a4b62d4a
Gustavo Barrera <gustavobarrera1@outlook.es>
2026-06-24 18:36:20 -0300

    add github actions deploy

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,10 @@
 terraform {
 
   backend "s3" {
-    bucket = "flaskapp-tfstate-1"
-    key = "dev/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket       = "flaskapp-tfstate-1"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
   }
 
